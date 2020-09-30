00ce1487c77f11e5b8dec5ce2cde0c336a304a2d
Ximena Vega <ximena.garcia7482@alumnos.udg.mx>
2020-09-29 21:38:02 -0500

    Optimización del código

--- Sniffer.cpp
+++ Sniffer.cpp
@@ -3,67 +3,62 @@
 
 using namespace std;
 
-int icar = 0;
 void imprimir(int, int, string, int, unsigned char);
 void tipoDeCodigo(unsigned char, int);
 
 int main()
 {
-    FILE *archivo;
-    unsigned char car;
-    int bytes = 1;
+    FILE *archivo = fopen("ethernet_3.bin", "rb");
+    unsigned char dato;
+    int bytesCont = 1;
 
-    if ((archivo = fopen("ethernet_3.bin", "rb")) == NULL)
+    if(archivo == NULL)
        printf ("Error en la apertura. Es posible que el fichero no exista.\n");
     else
-        while(!feof(archivo))
-            {
-                fread(&car, sizeof(unsigned char), 1, archivo);
+        while(!feof(archivo)){
+            fread(&dato, sizeof(unsigned char), 1, archivo);
 
-                imprimir(1, 6, "Direccion de origen: ", bytes, car);
-                imprimir(7, 12, "Direccion de destino: ", bytes, car);
-                imprimir(13, 14, "Tipo: ", bytes, car);
-                imprimir(15, 1517, "Datos: ",bytes, car);
-                bytes++;
-            }
+            imprimir(1, 6, "Direccion de origen: ", bytesCont, dato);
+            imprimir(7, 12, "Direccion de destino: ", bytesCont, dato);
+            imprimir(13, 14, "Tipo: ", bytesCont, dato);
+            imprimir(15, 1517, "Datos: ",bytesCont, dato);
+            bytesCont++;
+        }
     fclose (archivo);
     return (0);
 }
 
-void imprimir(int min, int max, string campo, int bytes, unsigned char car){
+void imprimir(int min, int max, string campo, int bytesCont, unsigned char dato){
 
-    if(bytes >= min && bytes <= max){
-        if(bytes == min)
-            cout<<campo;
+    if(bytesCont >= min && bytesCont <= max){
+        if(bytesCont == min)
+            cout << campo;
 
-        if(bytes<13)
-            printf("%02X:", car & 0xFF);
-        else
-            printf("%02X ", car & 0xFF);
+        printf("%02X ", dato & 0xFF);
 
-        if(bytes == 13 || bytes ==14)
-            tipoDeCodigo(car, bytes);
+        if(bytesCont == 13 || bytesCont ==14)
+            tipoDeCodigo(dato, bytesCont);
 
-        if(bytes == max)
+        if(bytesCont == max)
             printf("\b \n");
     }
 }
 
-void tipoDeCodigo(unsigned char car, int bytes){
-    icar += car;
+void tipoDeCodigo(unsigned char dato, int bytesCont){
+    int idato += dato;
 
-    if(bytes == 14){
-
-        if(icar == 14)
-            printf("-> ARP ");
-
-        if(icar == 181)
-            printf("-> RARP ");
-
-        if(icar == 8)
+    switch(bytesCont){
+        case 8:
             printf("-> IPv4 ");
-
-        if(icar == 355)
+            break;
+        case 14:
+            printf("-> ARP ");
+            break;
+        case 181:
+            printf("-> RARP ");
+            break;
+        case 255:
             printf("-> IPv6 ");
+
     }
 }
